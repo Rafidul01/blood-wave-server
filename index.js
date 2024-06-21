10f0b3c98994b41cb5c2d7b32f110cc3c1c55696
Rafidul01 <rafidulhasan555@gmail.com>
2024-06-21 23:03:01 +0600

    single request get api created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -98,9 +99,27 @@ async function run() {
     app.get("/requests", async (req, res) => {
         const email = req.query?.email;
         const filter = req.query?.status;
-        const query = { requesterEmail: email , status: filter };      
+        if(!filter){
+            const query = { requesterEmail: email };
+            const requests = await requestCollection.find(query).toArray();
+            res.send(requests);
+        }
+        else{
+          const query = { requesterEmail: email , status: filter }; 
+
         const requests = await requestCollection.find(query).toArray();
         res.send(requests);
+
+        }
+        
+    })
+
+    app.get("/request/:id", async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const query = { _id: new ObjectId(id) };
+        const request = await requestCollection.findOne(query);
+        res.send(request);
     })
 
     // Connect the client to the server	(optional starting in v4.7)
