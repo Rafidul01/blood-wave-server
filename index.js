26c4822ab0bef85c9a062023855a86f2f067c89e
Rafidul01 <rafidulhasan555@gmail.com>
2024-06-22 12:02:34 +0600

    jwt added with admin and volunteer varification

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { ObjectId } = require('mongodb');
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -32,6 +33,50 @@ async function run() {
     const upazilaCollection = client.db("bloodWave").collection("upazila");
     const userCollection = client.db("bloodWave").collection("users");
     const requestCollection = client.db("bloodWave").collection("requests");
+
+    // JWT related Api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    // Middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+    const verifyVolunteer = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isVolunteer = user.role === 'volunteer';
+      if (!isVolunteer) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+  }
 
     // district and upazila api
     app.get("/districts", async (req, res) => {
@@ -106,6 +151,37 @@ async function run() {
         const result = await userCollection.updateOne(query, updatedUser, option);
         res.send(result);
     })
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin  });
+  })
+
+  app.get('/users/volunteer/:email', verifyToken, verifyVolunteer, async (req, res) => {
+    const email = req.params.email;
+    if (email !== req.decoded.email) {
+      return res.status(403).send({ message: 'forbidden access' });
+    }
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    let volunteer = false;
+    if (user) {
+      volunteer = user?.role === 'volunteer';
+    }
+    res.send({ volunteer  });
+  })
+
+
+
 
     // request api
     
